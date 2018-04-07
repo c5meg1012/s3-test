c811e0db31eea8ee919bbe98020175fa59e05804
c5meg1012 <c5meg1012@gmail.com>
2018-04-07 22:56:10 +0900

    pre-signed urlへファイルアップロード

--- download_from_pre_signed_url.rb
+++ download_from_pre_signed_url.rb
@@ -3,7 +3,7 @@ require "./settings"
 # オブジェクトの取得
 object = $s3.bucket($bucket).object("jpg/20180407214211_polidog.jpg")
 
-# pre-signed urlの取得してURIオブジェクトにする
+# pre-signed urlを取得してURIオブジェクトにする
 # :expires_inで、有効期間(sec)を指定。デフォルト900sec。MAX 1week(604800sec)
 pre_signed_url = URI.parse(object.presigned_url(:get, expires_in: 30))
 
--- upload_to_pre_signed_url.rb
+++ upload_to_pre_signed_url.rb
@@ -1,1 +1,33 @@
 require "./settings"
+
+require "net/http"
+
+# ファイル
+file_path = "./images/polidog.jpg"
+
+# 拡張子
+file_extname = File.extname(file_path).delete!(".")
+
+# ファイル名
+file_name = File.basename(file_path)
+
+# タイムスタンプ
+timestamp = DateTime.now.strftime('%Y%m%d%H%M%S')
+
+# アップロード先
+object = $s3.bucket($bucket).object("#{file_extname}/#{timestamp}_#{file_name}")
+
+# pre-signed urlを取得
+presigned = object.presigned_post(server_side_encryption: "AES256")
+
+# アップロード
+cmd = "curl -v -X POST"
+presigned.fields.each do |k, v|
+  cmd << " -F \"#{k}=#{v}\""
+end
+cmd << " -F file=@#{file_path}"
+cmd << " "
+cmd << presigned.url
+
+puts cmd
+puts `#{cmd}`
